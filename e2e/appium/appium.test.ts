47327dcfe49a2969fa37e95855cdbfde85184149
Gustavo Silveira <gustavos60@gmail.com>
2023-05-20 18:41:49 -0300

    feat: fixed ios build settings

--- e2e/appium/appium.test.ts
+++ e2e/appium/appium.test.ts
@@ -2,13 +2,14 @@ import wdio from 'webdriverio';
 import {SignInTestIds} from '../../src/screens/signin/SignInConstants';
 import {ANDROID_CAPABILITIES, IOS_CAPABILITIES} from './capabilities';
 import {byAccessibilityLabel} from './utils';
+import {HomeTestIds} from '../../src/screens/home/HomeConstants';
 
 const opts = {
   path: '/wd/hub',
   port: 4723,
 };
 
-describe('appium test', () => {
+describe('Appium', () => {
   jest.setTimeout(15000);
   let client: WebdriverIO.Browser;
 
@@ -32,29 +33,51 @@ describe('appium test', () => {
     }
   });
 
-  it('should login successfully', async () => {
-    const emailInput = await client.$(
-      byAccessibilityLabel(SignInTestIds.emailInput),
-    );
-    await emailInput.click();
-    await emailInput.sendKeys(['eve.holt@reqres.in']);
+  describe('Authentication', () => {
+    it('should login successfully', async () => {
+      const emailInput = await client.$(
+        byAccessibilityLabel(SignInTestIds.emailInput),
+      );
+      await emailInput.click();
+      await emailInput.sendKeys(['eve.holt@reqres.in']);
 
-    const passwordInput = await client.$(
-      byAccessibilityLabel(SignInTestIds.passwordInput),
-    );
-    await passwordInput.click();
-    await passwordInput.sendKeys(['cityslicka']);
+      const passwordInput = await client.$(
+        byAccessibilityLabel(SignInTestIds.passwordInput),
+      );
+      await passwordInput.click();
+      await passwordInput.sendKeys(['cityslicka']);
 
-    const mainButton = await client.$(
-      byAccessibilityLabel(SignInTestIds.mainButton),
-    );
-    await mainButton.click();
+      const mainButton = await client.$(
+        byAccessibilityLabel(SignInTestIds.mainButton),
+      );
+      await mainButton.click();
 
-    await client.waitUntil(async () =>
-      (await client.$(byAccessibilityLabel('HomeScreen'))).isDisplayed(),
-    );
+      await client.waitUntil(async () =>
+        (
+          await client.$(byAccessibilityLabel(HomeTestIds.headerText))
+        ).isDisplayed(),
+      );
 
-    const homeScreenText = await client.$(byAccessibilityLabel('HomeScreen'));
-    expect(homeScreenText.isDisplayed()).toBeTruthy();
+      const homeScreenText = await client.$(
+        byAccessibilityLabel(HomeTestIds.headerText),
+      );
+      expect(await homeScreenText.isDisplayed()).toBeTruthy();
+    });
+
+    it('should logout successfully', async () => {
+      (await client.$(byAccessibilityLabel(HomeTestIds.signOut))).click();
+
+      await client.waitUntil(async () =>
+        (
+          await client.$(byAccessibilityLabel(SignInTestIds.welcomeText))
+        ).isDisplayed(),
+      );
+
+      const welcomeText = await client.$(
+        byAccessibilityLabel(SignInTestIds.welcomeText),
+      );
+
+      expect(await welcomeText.isDisplayed()).toBeTruthy();
+    });
   });
 });
